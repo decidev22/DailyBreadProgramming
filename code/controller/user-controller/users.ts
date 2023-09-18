9a274a018d7494c8e438a49ab5f21647f3f7dc4a
decidev22 <hamento22@gmail.com>
2023-09-18 21:27:02 +1200

    getUserById

--- code/controller/user-controller/users.ts
+++ code/controller/user-controller/users.ts
@@ -37,6 +37,20 @@ export const get_UserByEmail = async (
   }
 };
 
+export const get_UserById = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const { id } = req.params;
+    const user = await getUserById(id);
+    return res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
 export const deleteUser = async (
   req: express.Request,
   res: express.Response
